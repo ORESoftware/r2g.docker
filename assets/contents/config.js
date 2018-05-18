a37ba91c30b45d1bb7c82bc895a2ea0ab36607e5
Olegzandr VD <alex@oresoftware.com>
2018-05-18 16:45:24 -0700

    pdev:'set'

--- assets/contents/config.js
+++ assets/contents/config.js
@@ -1,10 +1,15 @@
 
 const path = require('path');
 
+if(!path.isAbsolute(process.env.MY_DOCKER_R2G_SEARCH_ROOT || '')){
+  throw new Error('Please set the env var "MY_DOCKER_R2G_SEARCH_ROOT" to an absolute path.');
+}
+
+
 exports.default = {
   
   
-  searchRoot: path.resolve(process.env.HOME),
+  searchRoot: path.resolve(process.env.MY_DOCKER_R2G_SEARCH_ROOT),
   
   // the following packages will be installed in the Docker container using this pattern:
   
@@ -13,12 +18,8 @@ exports.default = {
   
   packages: {
   
-    // absolute local path (perhaps, if you have two projects with the same package.json name on your disk)
-    example1: "/absolute/local/path/to/project/root",
-    
-    // it's truthy non-string it will be discovered on your disk
-    example2: true,
-    
+    'example1': true,
+    'example2': true,
     // if it's falsy it will be ignored
     '@org/example3': 0
     
