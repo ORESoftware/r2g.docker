a55fdd304eb61e70903135e6e8190e719f3143fa
Olegzandr VD <alex@oresoftware.com>
2018-05-17 19:23:11 -0700

    pdev:'set'

---
+++ src/commands/run/rename-file-deps.ts
@@ -0,0 +1,83 @@
+import cp = require('child_process');
+import path = require("path");
+import fs = require('fs');
+import async = require('async');
+import log from "../../logger";
+import * as util from "util";
+
+/////////////////////////////////////////////////////////////////
+
+export const installDeps = function (createProjectMap: any, pkgJSONPath: string, cb: any) {
+  
+  async.autoInject({
+    
+    rereadPkgJSON: function (cb: Function) {
+      
+      fs.readFile(pkgJSONPath, function (err, data) {
+        
+        if (err) {
+          return cb(err);
+        }
+        try {
+          return cb(null, JSON.parse(String(data)));
+        }
+        catch (err) {
+          return cb(err);
+        }
+        
+      })
+      
+    },
+    
+    saveNewJSONFileToDisk: function (rereadPkgJSON: any, cb: Function) {
+      
+      const updateTheDepKV = function () {
+        [
+          rereadPkgJSON.dependencies,
+          rereadPkgJSON.devDependencies,
+          rereadPkgJSON.optionalDependencies
+        
+        ]
+        .forEach(function (d) {
+          
+          d = d || {};
+          
+          Object.keys(d).forEach(function (k) {
+            
+            const v = d[k];
+            
+            if (String(v).startsWith('file://')) {
+              
+              if (createProjectMap[k]) {
+                d[k] = 'file://' + createProjectMap[k];
+              }
+              else {
+                log.error('The following dep has a file:// key, but does not exist in generated map => ' + k);
+                throw 'Please check your package.json file: ' + util.inspect(rereadPkgJSON)
+              }
+              
+            }
+            
+          });
+          
+        });
+      };
+      
+      let str = null;
+      
+      try {
+        updateTheDepKV();
+        str = JSON.stringify(rereadPkgJSON, null, 2);
+      }
+      catch (err) {
+        return cb(err);
+      }
+      
+      // save the json object back to disk
+      fs.writeFile(pkgJSONPath, str, cb);
+      
+    }
+    
+  }, cb);
+  
+};
