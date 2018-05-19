0d9bcdbe65162181d901d6e563b4f748234c6793
Olegzandr Denman <alex@oresoftware.com>
2018-05-18 17:27:12 -0700

    pdev:'set'

--- .docker.r2g/config.js
+++ .docker.r2g/config.js
@@ -2,21 +2,21 @@ const path = require('path');
 
 
 if(!path.isAbsolute(process.env.MY_DOCKER_R2G_SEARCH_ROOT || '')){
-  throw new Error('Please the env var MY_DOCKER_R2G_SEARCH_ROOT to an absolute path.');
+  throw new Error('Please set the env var MY_DOCKER_R2G_SEARCH_ROOT to an absolute path.');
 }
 
 exports.default = {
-  
-  
+
+
   searchRoot: path.resolve(process.env.MY_DOCKER_R2G_SEARCH_ROOT),
   // searchRoot: path.resolve(process.env.HOME + '/WebstormProjects/oresoftware'),
-  
+
   // the following packages will be installed in the Docker container using this pattern:
-  
+
   // npm install /r2g_shared_dir/Users/you/
-  
+
   packages: {
-    
+
     // absolute local path (perhaps, if you have two projects with the same package.json name on your disk)
     // example1: "/absolute/path/to/project/root",
     //
@@ -25,10 +25,10 @@ exports.default = {
     //
     // // if it's falsy it will be ignored
     // '@org/example3': 0
-    
+
     residence: true,
     // r2g: false
-    
+
   }
-  
+
 };
