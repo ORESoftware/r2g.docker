10b4a8af0f6cd9f3c312ca1792bbaf7e73da1889
Olegzandr Denman <alex@oresoftware.com>
2018-06-22 02:54:57 -0700

    pdev:'set'

--- assets/cli.sh
+++ assets/cli.sh
@@ -12,6 +12,10 @@ commands="$basic_path/dist/commands"
 cmd="$1";
 shift 1;
 
+if [ -z "$(which docker)" ]; then
+  echo >&2 "docker does not appear to be installed on your system (the docker executable cannot be found using 'which').";
+fi
+
 
 if [ "$cmd" == "init" ]; then
   node "$commands/init" $@
