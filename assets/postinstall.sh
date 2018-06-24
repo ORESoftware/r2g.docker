f9a74ff8a095c75b122b06d1a065aa7b6b8d9772
Olegzandr Denman <alex@oresoftware.com>
2018-06-24 00:22:01 -0700

    pdev:'set'

--- assets/postinstall.sh
+++ assets/postinstall.sh
@@ -2,19 +2,19 @@
 
 set -e;
 
-if [[ "$read_json_skip_postinstall" == "yes" ]]; then
-   echo "read.json is skipping postinstall routine.";
+if [[ "$dkr2g_skip_postinstall" == "yes" ]]; then
+   echo "dkr2g is skipping postinstall routine.";
    exit 0;
 fi
 
-export read_json_skip_postinstall="yes";
+export dkr2g_skip_postinstall="yes";
 
-gmx_gray='\033[1;30m'
-gmx_magenta='\033[1;35m'
-gmx_cyan='\033[1;36m'
-gmx_orange='\033[1;33m'
-gmx_green='\033[1;32m'
-gmx_no_color='\033[0m'
+r2g_gray='\033[1;30m'
+r2g_magenta='\033[1;35m'
+r2g_cyan='\033[1;36m'
+r2g_orange='\033[1;33m'
+r2g_green='\033[1;32m'
+r2g_no_color='\033[0m'
 
 
 mkdir -p "$HOME/.oresoftware" || {
@@ -101,7 +101,7 @@ mkdir -p "$HOME/.oresoftware/nodejs/node_modules" ||{
 
 
 echo "";
-echo -e "${gmx_green} => dkr2g was installed successfully.${gmx_no_color}";
+echo -e "${r2g_green} => dkr2g was installed successfully.${r2g_no_color}";
 echo -e "Add the following line to your .bashrc/.bash_profile files:";
-echo -e "${gmx_cyan}. \"\$HOME/.oresoftware/shell.sh\"${gmx_no_color}";
+echo -e "${r2g_cyan}. \"\$HOME/.oresoftware/shell.sh\"${r2g_no_color}";
 echo "";
--- assets/shell.sh
+++ assets/shell.sh
@@ -1,14 +1,21 @@
 #!/usr/bin/env bash
 
-
+get_latest_dkr2g(){
+  . "$HOME/.oresoftware/bash/dkr2g.sh";
+}
 
 dkr2g(){
 
-  if [[ -z $(command -v dkr2g) ]] || [[ -z $(which dkr2g) ]]; then
-       npm install -g --silent '@oresoftware/docker.r2g'  || {
+ if ! type -f dkr2g &> /dev/null || ! which dkr2g &> /dev/null; then
+
+       echo -e "Installing dkr2g command line package from NPM..." >&2;
+
+       npm i -s -g '@oresoftware/docker.r2g'  || {
+
           echo -e "Could not install the '@oresoftware/r2g' NPM package globally." >&2;
           echo -e "Check your user permissions to install global NPM packages." >&2;
           return 1;
+
       }
   fi
 
