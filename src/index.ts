0dadd169ebad07baea078eede561ceb1b02adf19
Operations Research Engineering Software+ <alex@oresoftware.com>
2018-11-29 18:20:26 -0800

    ores/auto-commit => 'set'

--- src/index.ts
+++ src/index.ts
@@ -2,22 +2,8 @@
 
 import ProcessEnv = NodeJS.ProcessEnv;
 
-// export interface MyProcessEnv extends ProcessEnv {
-//   r2g_container_id: string,
-//   docker_r2g_is_debug: string
-// }
-
 declare global {
-
-  // declare namespace NodeJS {
-  //   export interface EnvironmentVariables {
-  //     r2g_container_id: string,
-  //     docker_r2g_is_debug: string
-  //   }
-  // }
-
   namespace NodeJS {
-
     export interface ProcessEnv  {
       [key:string]: string,
       r2g_container_id: string,
@@ -25,22 +11,9 @@ declare global {
       docker_r2g_fs_map: string
       HOME: string
     }
-
-    // export interface Global {
-    //    process: {
-    //      env: MyProcessEnv
-    //    }
-    // }
   }
-
 }
-//
-// declare var process : {
-//   env: {
-//           r2g_container_id: string,
-//       docker_r2g_is_debug: string
-//   }
-// };
+
 
 export type ErrorValueCallback = (err: any, val: any) => void;
 
